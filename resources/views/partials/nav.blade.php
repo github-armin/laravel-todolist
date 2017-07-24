1dd096cfb2a8de6f2969667e8ff5955190eff426
Armin <kzardkoohi@gmail.com>
2017-07-24 14:29:56 -0500

    cleaned up nav bar

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -8,14 +8,12 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="/"><i class="fa fa-list-ol"></i></a>
+          <a class="navbar-brand" href="/"><i class="fa fa-list-ol"></i>&nbsp;&nbsp;Todo List</a>
         </div>
         <div id="navbar" class="collapse navbar-collapse">
-          <ul class="nav navbar-nav">
-            <li><a href="#">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
-          </ul>
+          <!--<ul class="nav navbar-nav">
+            <li><a href="/">Todo List</a></li>
+          </ul>-->
           <ul class="nav navbar-nav navbar-right">
             <form class="navbar-form navbar-right" method="get" action="todo/create">
             <button type="submit" class="btn btn-success"><i class="fa fa-plus"></i></button>
