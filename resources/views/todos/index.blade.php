f7e6bb063a7a0ae6cb3d75a39b388d4b50319279
Armin <kzardkoohi@gmail.com>
2017-07-24 13:56:42 -0500

    integrated with main template layout

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -1,10 +1,15 @@
+@extends('layouts.main')
+
+@section('content')
+
 <h1>Todos List</h1>
 
 @if(count($todos) > 0)
   @foreach($todos as $todo)
     <div class="well">
-      <h3>{{$todo->text}}</h3>
-      <span class="label label-danger">{{$todo->due}}</span>
+      <h4>{{$todo->text}} <span class="label label-danger">{{$todo->due}}</span></h4>
     </div>
   @endforeach
 @endif
+
+@endsection
